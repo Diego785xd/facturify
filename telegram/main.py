ccd91ade9fab787067fce90beb22c438fa72edf0
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-04-14 03:59:09 -0600

    Update main.py

--- telegram/main.py
+++ telegram/main.py
@@ -1,3 +1,6 @@
+import asyncio
+import io
+import requests
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, Updater, CommandHandler, CallbackContext, ContextTypes, MessageHandler, filters
@@ -11,7 +14,27 @@ async def start_command(update: Update, context: CallbackContext):
 async def help_command(update: Update, context: CallbackContext):
     await update.message.reply_text('Puedo ayudarte con las siguientes acciones: \n\n /registro - Registrate en la plataforma de Facturify \n /subir_ticket - Sube un nuevo ticket a la plataforma y empieza el proceso de facturación de este mismo. \n /muestrame_mis_facturas - Te doy una lista de las facturas que tienes actualmente según la lista de parametros que me pidas.')
 
+async def handle_image(update: Update, context: CallbackContext):
+    URL = 'http://127.0.0.1:8080/tickets'
+        
+    try:
+        photo_file = await update.message.photo[-1].get_file()
+        # Descargar la imagen localmente
+        photo_file_path = 'temp_image.jpg'
+        await photo_file.download_to_drive(photo_file_path)
 
+        # Enviar la imagen como multipart/form-data
+        with open(photo_file_path, 'rb') as file:
+            files = {'image': file}
+            response = requests.post(URL, files=files)
+
+        # Verificar si la petición fue exitosa
+        if response.status_code == 200:
+            await update.message.reply_text('Ticket subido correctamente.')
+        else:
+            await update.message.reply_text('Hubo un error al subir el ticket.')
+    except Exception as e:
+        print(e)    
 
 # Responses
 async def handle_response(text:str)-> str:
@@ -30,7 +53,7 @@ async def handle_message(update: Update, context: CallbackContext):
 
     print(f'User ({update.message.chat.id}) in {message_type}')
     
-    if message_type == 'group':
+    if message_type == 'private':
         if BOT_USERNAME in text:
             await update.message.reply_text('Hola, ¿en qué puedo ayudarte?')
         else:
@@ -49,9 +72,10 @@ if __name__ == '__main__':
     #Commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
-
-    # Messages
-    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # Images 
+    app.add_handler(MessageHandler(filters.PHOTO, handle_image))
+    
 
     # Error
     app.add_error_handler(error)
