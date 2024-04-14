c8df19f33f66be8cdb3551d98cf02bce8cd10016
Oscar Santos <oscarsantosmu@gmail.com>
2024-04-14 04:07:47 -0600

    fix: start up

--- src/fastapi/app/db/firestore.py
+++ src/fastapi/app/db/firestore.py
@@ -1,8 +1,14 @@
+import os
+from dotenv import load_dotenv
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("firebasecreds.json")
+# read credentials from environment variable with a value of a path to the json file
+load_dotenv()
+
+cred = credentials.Certificate(os.environ.get("FIREBASE_CREDS_PATH"))
 firebase_admin.initialize_app(cred)
 
 
